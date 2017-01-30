496c9e715c6e4995c57681f86b164aeb520f2a02
archendian <arch.endian@gmail.com>
2017-01-30 07:07:25 -0500

    Move Ngen.Math from future to now
    Prerequisite to the IME and GRE

--- develop/now/Ngen.Math/include/Ngen.Math.Vector2.hpp
+++ develop/now/Ngen.Math/include/Ngen.Math.Vector2.hpp
@@ -1,4 +1,31 @@
+/*  _______    ________
+    \      \  /  _____/  ____   ___
+    /   |   \/   \  ____/ __ \ /   \
+   /    |    \    \_\  \  ___/|   | \
+   \____|__  /\______  /\___  >___| /
+           \/        \/     \/    \/
+The MIT License (MIT)
 
+COPYRIGHT (C) 2017 FIXCOM, LLC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 #ifndef _NGEN_MATH_VECTOR2
 #define _NGEN_MATH_VECTOR2
 
@@ -272,7 +299,6 @@ namespace Ngen {
     template<typename T>
     class Factory {
     public:
-
         virtual T* GetInstance(string id) const pure;
         virtual void DeleteInstance(string id) pure;
         virtual T* NewInstance(string id, ...) pure;
@@ -280,9 +306,8 @@ namespace Ngen {
         virtual T* LoadInstance(const string& id, Stream* in) pure;
         virtual bool SaveInstance(const string& id, Stream* out) pure;
         virtual T* CopyInstance(const string& id, const string& copyId) pure;
-    protected:
-        string mId;
     };
+
 
     class SceneNode;
 

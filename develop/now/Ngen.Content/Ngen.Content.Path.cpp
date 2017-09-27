c6d346f3716ae7f68b2012298a7561d9a634058c
arch.endian <arch.endian@gmail.com>
2017-09-27 18:52:04 -0400

    Update Ngen.Content.Path.cpp

--- develop/now/Ngen.Content/Ngen.Content.Path.cpp
+++ develop/now/Ngen.Content/Ngen.Content.Path.cpp
@@ -1,1 +1,23 @@
 
+
+
+#include <Ngen.hpp>
+
+namespace Ngen {
+  namespace Content {
+     _static tchar Path::PathSeperatorChar() {
+       #if _tkn_Platform == _tknval_Platform_Windows 
+         return '/';
+       #else
+         return '/';
+       #endif
+     }
+  }
+}
+class Path {
+      public:
+        
+        static const string& SystemPathChar() const;
+        static string GetLastNode(const string& node=const_string("/\|"));
+    };
+}
